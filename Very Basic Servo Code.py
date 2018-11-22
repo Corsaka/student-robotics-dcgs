73dab5affeca181d89e58f712deab73a7c1995d1
CrazyGaming77 <luke.amos@challoners.org>
2018-11-22 20:39:28 +0000

    Update Very Basic Servo Code.py

--- Very Basic Servo Code.py
+++ Very Basic Servo Code.py
@@ -2,6 +2,6 @@ from sr.robot import *
 from time import sleep
 R = Robot()
 
-def servoWiggle:
+def servoWiggle():
   R.servos[0][1] = 0 #pos = 0
   R.servos[0][1]=(R.servos[0][1])+5 #turn 5* clockwise (dont turn more than 5* at a time or you'll break the servo) #goes from -100 to 100
