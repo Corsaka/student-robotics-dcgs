9d0f8729bdb974fa69a33e44071a1b45a349f388
CrazyGaming77 <luke.amos@challoners.org>
2018-11-22 20:43:32 +0000

    Create Basic Webcam Code.py

---
+++ Basic Webcam Code.py
@@ -0,0 +1,24 @@
+#imports
+from sr.robot import *
+from time import sleep
+R = Robot()
+
+def markerSee():
+    markers = R.see() # makes a list of markers it sees
+    print("I can see"), len(markers), "markers:" #prints no. of markers
+    for m in markers: 
+        if m.info.marker_type in (MARKER_TOKEN_A, MARKER_TOKEN_B, MARKER_TOKEN_C):
+          print(" - Marker #{0} is {1} metres away".format( m.info.code, m.dist )) #prints what markers are how far away
+
+#errormarkers = R.see( res=(1280,1024) ) #chooses resolution of 1280x1024
+#The Logitech C500 has a field of view of 72° and the C270 has a field of view of 60°.
+
+'''
+#####OTHER WEBCAM COMMANDS#####
+##These 3 print coordinates of a visiple point, p:
+print("length", m.info.code.polar.length) - 
+print("rot_x", m.info.code.polar.rot_x)
+print("rot_y", m.info.code.polar.rot_y) 
+
+##other commands aren't explained in use, so will take some figuring out
+'''
