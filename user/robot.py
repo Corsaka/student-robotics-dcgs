e4a6828caa2e888772c826e686a9d6ba5881ca01
CrazyGaming77 <luke.amos@challoners.org>
2018-11-23 18:07:02 +0000

    Update robot.py

--- user/robot.py
+++ user/robot.py
@@ -1,38 +1,35 @@
-print("Robot online!")
-# did the script work?
 from sr.robot import *
 from time import sleep
 R = Robot.setup()
-#imports
 
 class CustomisedRuggeduino(Ruggeduino):
     def ultrasonicSensor(self):
         with self.lock:
             distance = self.command("u")
-            print "Distance: " + str(distance) + " cm away"
-            return distance
-        # prints distance away from wherever the camera is pointed
-
-if command=='turn': #positive is left, negative is right, zero stops it 
-        frontleft.power,backleft.power,frontright.power,backright.power = -speed,speed,speed,speed
-    if command=='straight':#positive is forward, negative is right, zero is stop
-        backright.power,backright.power,frontleft.power,backleft.power = speed,-speed,speed,speed
-    # defined by the while loop below
-    # and then adjusts the speed of the motors accordingly
-
+            print(distance)
+            if distance < 10:
+                R.power.beep(1000, 'a')
+                
 R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
 R.init()
 R.wait_start()
 
+def dualMotorTest():
+  print('Testing motor[0]:')
+  sleep(0.5)
+  R.motors[0].m0.power = 100
+  R.motors[0].m1.power = 100
+  sleep(1.5)
+  R.motors[0].m0.power = 0
+  R.motors[0].m1.power = 0
+  print('Testing motor[1]:')
+  sleep(0.5)
+  R.motors[1].m0.power = -100
+  R.motors[1].m1.power = -100
+  sleep(1.5)
+  R.motors[1].m0.power = 0
+  R.motors[1].m1.power = 0
+  sleep(0.5)
+
 while True:
-    distanceAway = int(R.ruggeduinos["75230313833351618141"].ultrasonicSensor())
-    print str(distanceAway)
-    if distanceAway < 10:
-        move('straight',0)
-        # stops the robot
-    else:
-        move('straight',15)
-        # moves the robot forward
-# todo:
-# left/right turns
-# adjustable speed, perhaps based on distance
+  dualMotorTest()
