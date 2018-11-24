a507336d8abc83ce0305625df9bf55ff914208a6
CrazyGaming77 <luke.amos@challoners.org>
2018-11-24 17:16:19 +0000

    Update Basic Movement.py

--- Basic Movement.py
+++ Basic Movement.py
@@ -28,7 +28,7 @@ def basicMovement():
   R.motors[1].m0.power = -100
   R.motors[1].m1.power = -100
   sleep(1)
-   R.ruggeduinos["75230313833351618141"].gyroTurn()
+  R.ruggeduinos["75230313833351618141"].gyroTurn()
 
 while True:
   basicMovement()
