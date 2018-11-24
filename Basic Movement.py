0916e340f51c9eac79022a34c01919eeacc9462e
CrazyGaming77 <luke.amos@challoners.org>
2018-11-24 16:45:15 +0000

    Update Basic Movement.py

--- Basic Movement.py
+++ Basic Movement.py
@@ -9,6 +9,10 @@ class CustomisedRuggeduino(Ruggeduino):
             print(distance)
             if distance < 10:
                 R.power.beep(1000, 'a')
+    def gyroTurn(self):
+        with self.lock:
+            gyro = self.command("t")
+            print(gyro)
                 
 R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
 R.init()
@@ -19,10 +23,8 @@ def basicMovement():
   R.motors[0].m1.power = 100
   R.motors[1].m0.power = -100
   R.motors[1].m1.power = -100
-  sleep(3)
-  R.motors[1].m0.power = 100
-  R.motors[1].m1.power = 100
-  sleep(1.2)
+  sleep(1)
+  gyroTurn()
 
 while True:
   basicMovement()
