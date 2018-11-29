a64e1c927fd0209063160aea3ff376e28f7b647d
CrazyGaming77 <luke.amos@challoners.org>
2018-11-29 12:34:05 +0000

    Create Gyro Response Time Test.py

---
+++ Gyro Response Time Test.py
@@ -0,0 +1,23 @@
+from sr.robot import *
+from time import sleep
+R = Robot.setup()
+
+angle = 0
+
+class CustomisedRuggeduino(Ruggeduino):
+    def ultrasonicSensor(self):
+        with self.lock:
+            distance = self.command("u")
+            print(distance)
+            if distance < 10:
+                R.power.beep(1000, 'a')    
+    def getAngle(self):
+        with self.lock:
+            gyro = int(round(float(self.command("t").replace('\n', ''))))
+            return gyro
+            
+for i in range(0, 500):
+  angle += R.ruggeduinos["75230313833351618141"].getAngle()
+
+newAngle = angle/500
+print('Done: %d' % (newAngle))
