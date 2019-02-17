9ee73dd1ca7f5b872723d0baa278b982a8f2c5ba
CrazyGaming77 <luke.amos@challoners.org>
2019-02-17 13:27:23 +0000

    Update Basic Movement with Gyro.py

--- Basic Movement with Gyro.py
+++ Basic Movement with Gyro.py
@@ -23,7 +23,7 @@ def turnAround(angle):
   R.motors[1].m0.power = 50
   R.motors[1].m1.power = 50
   while float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', '')) <= angle + 180:
-     sleep(0.01)
+     sleep(0.0001)
   R.motors[0].m0.power = 0
   R.motors[0].m1.power = 0
   R.motors[1].m0.power = 0
