d309c5161e1b8499e0ca0b31de0e75c2e1d4c837
CrazyGaming77 <luke.amos@challoners.org>
2019-02-22 19:35:04 +0000

    Update Ultrasonic Sensor Test.py

--- Ultrasonic Sensor Test.py
+++ Ultrasonic Sensor Test.py
@@ -6,8 +6,12 @@ class CustomisedRuggeduino(Ruggeduino):
     def ultrasonicSensor(self):
         with self.lock:
             distance = self.command("u")
+            print(distance)
             if distance < 10:
-                R.power.beep(1000, 'a')
+                R.power.beep(1000, 'a')    
+    def getAngle(self):
+        with self.lock:
+            return self.command("t")
                 
 R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
 ultrasonicSensor = R.ruggeduinos["75230313833351618141"].ultrasonicSensor()
