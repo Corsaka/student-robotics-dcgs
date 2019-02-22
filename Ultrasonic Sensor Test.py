499838241bcc66f50a8a1b5de31f19cfb0b8d538
CrazyGaming77 <luke.amos@challoners.org>
2019-02-22 19:29:06 +0000

    Create Ultrasonic Sensor Test.py

---
+++ Ultrasonic Sensor Test.py
@@ -0,0 +1,16 @@
+from sr.robot import *
+from time import sleep
+R = Robot.setup()
+
+class CustomisedRuggeduino(Ruggeduino):
+    def ultrasonicSensor(self):
+        with self.lock:
+            distance = self.command("u")
+            if distance < 10:
+                R.power.beep(1000, 'a')
+                
+R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
+R.init()
+R.wait_start()
+
+R.ruggeduinos["75230313833351618141"].ultrasonicSensor()
