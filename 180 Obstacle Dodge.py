57f00befae05ba9b1b80234a90a2918a66beca3a
CrazyGaming77 <luke.amos@challoners.org>
2019-02-22 18:38:28 +0000

    Update 180 Obstacle Dodge.py

--- 180 Obstacle Dodge.py
+++ 180 Obstacle Dodge.py
@@ -1,1 +1,16 @@
+from sr.robot import *
+from time import sleep
+R = Robot.setup()
+
+class CustomisedRuggeduino(Ruggeduino):
+    def ultrasonicSensor(self):
+        with self.lock:
+            distance = self.command("u")
+            print(distance)
+            if distance < 10:
+                R.power.beep(1000, 'a')
+                
+R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
+R.init()
+R.wait_start()
 
