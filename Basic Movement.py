29ea16ed7ca8ccac6a7282f8ffe54192be966956
CrazyGaming77 <luke.amos@challoners.org>
2019-02-22 19:19:09 +0000

    Update Basic Movement.py

--- Basic Movement.py
+++ Basic Movement.py
@@ -8,27 +8,25 @@ class CustomisedRuggeduino(Ruggeduino):
             distance = self.command("u")
             print(distance)
             if distance < 10:
-                R.power.beep(1000, 'a')
-    def gyroSetup(self):
+                R.power.beep(1000, 'a')    
+    def getAngle(self):
         with self.lock:
-            self.command("b")
-    def gyroTurn(self):
-        with self.lock:
-            gyro = self.command("t")
-            print(gyro)
+            return self.command("t")
                 
 R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
 R.init()
 R.wait_start()
-R.ruggeduinos["75230313833351618141"].gyroSetup()
 
-def basicMovement():
-  R.motors[0].m0.power = 100
-  R.motors[0].m1.power = 100
-  R.motors[1].m0.power = -100
-  R.motors[1].m1.power = -100
-  sleep(1)
-  R.ruggeduinos["75230313833351618141"].gyroTurn()
-
-while True:
-  basicMovement()
+def forwards(time):
+    R.motors[0].m0.power = -100
+    R.motors[0].m1.power = -100
+    R.motors[1].m0.power = 100
+    R.motors[1].m1.power = 100
+    sleep(time)
+   
+def backwards(time):
+    R.motors[0].m0.power = 100
+    R.motors[0].m1.power = 100
+    R.motors[1].m0.power = -100
+    R.motors[1].m1.power = -100
+    sleep(time)
