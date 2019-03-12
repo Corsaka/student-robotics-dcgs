b4c628b4dff82c67e1a14c409e62e9e7ba07ee52
CrazyGaming77 <luke.amos@challoners.org>
2019-03-12 12:35:35 +0000

    Update Actuator Test.py

--- Actuator Test.py
+++ Actuator Test.py
@@ -1,1 +1,13 @@
-#Yes
+from sr.robot import *
+from time import sleep
+R = Robot.setup()
+                
+R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
+R.init()
+R.wait_start()
+
+while True:
+  R.motors[0].m1.power = 50
+  sleep(1.5)
+  R.motors[0].m1.power = -50
+  sleep(1.5)
