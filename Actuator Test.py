504b768223066d424f67318618e0db8cd2198924
CrazyGaming77 <luke.amos@challoners.org>
2019-03-12 12:38:40 +0000

    Update Actuator Test.py

--- Actuator Test.py
+++ Actuator Test.py
@@ -2,6 +2,11 @@ from sr.robot import *
 from time import sleep
 R = Robot.setup()
                 
+class CustomisedRuggeduino(Ruggeduino):  
+    def getAngle(self):
+        with self.lock:
+            return self.command("t")
+          
 R.ruggeduino_set_handler_by_fwver("SRcustom", CustomisedRuggeduino)
 R.init()
 R.wait_start()
