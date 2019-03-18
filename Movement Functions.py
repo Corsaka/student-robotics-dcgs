8c97b783336622083c3bd0310f04d1c8abbade42
CrazyGaming77 <luke.amos@challoners.org>
2019-03-18 12:45:35 +0000

    Update Movement Functions.py

--- Movement Functions.py
+++ Movement Functions.py
@@ -12,69 +12,49 @@ R.init()
 R.wait_start()
                    
 def forwards(time):
-  R.motors[0].m0.power = -100
   R.motors[0].m1.power = -100
-  R.motors[1].m0.power = 100
   R.motors[1].m1.power = 100
   sleep(time)
   
 def backwards(time):
-  R.motors[0].m0.power = 100
   R.motors[0].m1.power = 100
-  R.motors[1].m0.power = -100
   R.motors[1].m1.power = -100
   sleep(time)
 
 def left():
   angle = float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', ''))
-  R.motors[0].m0.power = 28
   R.motors[0].m1.power = 28
-  R.motors[1].m0.power = 28
   R.motors[1].m1.power = 28
   while float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', '')) <= angle + 90:
      sleep(0.00000000000000001)
-  R.motors[0].m0.power = 0
   R.motors[0].m1.power = 0
-  R.motors[1].m0.power = 0
   R.motors[1].m1.power = 0
   
 def right():
   angle = float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', ''))
-  R.motors[0].m0.power = -28
   R.motors[0].m1.power = -28
-  R.motors[1].m0.power = -28
   R.motors[1].m1.power = -28
   while float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', '')) > angle - 90:
      sleep(0.00000000000000001)
-  R.motors[0].m0.power = 0
   R.motors[0].m1.power = 0
-  R.motors[1].m0.power = 0
   R.motors[1].m1.power = 0
   
 def turnAround():
   angle = float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', ''))  
-  R.motors[0].m0.power = 28
   R.motors[0].m1.power = 28
-  R.motors[1].m0.power = 28
   R.motors[1].m1.power = 28
   while float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', '')) <= angle + 180:
      sleep(0.00000000000000001)
-  R.motors[0].m0.power = 0
   R.motors[0].m1.power = 0
-  R.motors[1].m0.power = 0
   R.motors[1].m1.power = 0     
  
 def turn(desired):
   angle = float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', ''))
-  R.motors[0].m0.power = 28
   R.motors[0].m1.power = 28
-  R.motors[1].m0.power = 28
   R.motors[1].m1.power = 28
   while float(R.ruggeduinos["75230313833351618141"].getAngle().replace('\n', '')) <= angle + desired:
      sleep(0.0000000000001)
-  R.motors[0].m0.power = 0
   R.motors[0].m1.power = 0
-  R.motors[1].m0.power = 0
   R.motors[1].m1.power = 0   
 R.ruggeduinos["75230313833351618141"].getAngle()
 R.ruggeduinos["75230313833351618141"].getAngle()
